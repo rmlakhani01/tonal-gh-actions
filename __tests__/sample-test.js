5749cc83e5baa0f177c60c073be5d13291731845
GhostDZ9 <rehan.lakhani@gmail.com>
2024-05-22 12:04:49 -0400

    adding jest sample test files

---
+++ __tests__/sample-test.js
@@ -0,0 +1,8 @@
+it('should assert strings are equal', () => {
+  const a = 'foobar'
+  const b = 'foobar'
+  // comment this line:
+  // expect(a).toMatch(b);
+  // and add this line:
+  expect(true).toBeFalsy()
+})
