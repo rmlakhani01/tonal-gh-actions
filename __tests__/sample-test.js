d14f897cde26aab6176eedbe3ec194d037e19468
GhostDZ9 <rehan.lakhani@gmail.com>
2024-05-22 12:07:17 -0400

    fixing sample test to get a succesful build

--- __tests__/sample-test.js
+++ __tests__/sample-test.js
@@ -2,7 +2,7 @@ it('should assert strings are equal', () => {
   const a = 'foobar'
   const b = 'foobar'
   // comment this line:
-  // expect(a).toMatch(b);
+  expect(a).toMatch(b)
   // and add this line:
-  expect(true).toBeFalsy()
+  //   expect(true).toBeFalsy()
 })
